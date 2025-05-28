7e4fa4c7b32a062172bd35fe08cabfdcdd1a9686
José Gois <ze.gois.00@gmail.com>
2025-05-28 18:42:13 -0300

    updated draft

--- draft/main.rs
+++ draft/main.rs
@@ -188,3 +188,52 @@
 
 //     // x86_64::syscall::execve(stack.argv.offset(1), stack.argv, stack.envp, auxv.entries);
 // }
+
+// pub extern "C" fn entry(stack_pointer: *mut u64) -> ! {
+//     xelf::info!("eXecuting Executable and Linkable Format\n");
+
+//     // Create a Stack instance from the provided pointer
+//     (0..10).for_each(|i| {
+//         info!("{:?}\n", unsafe { stack_pointer.add(i) });
+//     });
+//     let mut stack = arch::memory::Stack::from_pointer(arch::Pointer(stack_pointer));
+//     // xelf::info!("Stack from pointer:\n");
+//     // stack.print();
+
+//     // // Access specific stack elements
+//     // unsafe {
+//     //     if let Some(arg0) = stack.get_arg(0) {
+//     //         xelf::info!("Program name: {}\n", arg0);
+//     //     }
+
+//     //     // Check for specific arguments
+//     //     for i in 0..stack.argc {
+//     //         if let Some(arg) = stack.get_arg(i) {
+//     //             if arg == "/" {
+//     //                 xelf::info!("Found '/' at argument position {}\n", i);
+//     //             }
+//     //         }
+//     //     }
+
+//     //     // Get environment variables
+//     //     if let Some(path) = stack.get_env_by_name("PATH") {
+//     //         xelf::info!("PATH environment variable: {}\n", path);
+//     //     }
+
+//     //     // Get and modify auxiliary vector entries
+//     //     if let Some(entry_point) = stack.get_auxv_by_type(9) {
+//     //         // AT_ENTRY
+//     //         xelf::info!("Original entry point: {:#x}\n", entry_point);
+
+//     //         // Modify an auxiliary vector entry (example only, not actually changing it)
+//     //         if stack.set_auxv_by_type(9, 0x11502) {
+//     //             xelf::info!("Entry point updated (demonstrative only)\n");
+//     //         }
+//     //     }
+//     // }
+
+//     // xelf::info!("\nDemonstration complete\n");
+
+//     // stack.print();
+//     panic!("Stack demonstration completed successfully!");
+// }
--- draft/out/xelf/scripts/alias.sh
+++
@@ -1,2 +0,0 @@
-alias b="clear && cargo +nightly build -Z build-std=core --target x86_64-unknown-none.json --release"
-alias bw="RUSTFLAGS=-Awarnings && clear && cargo +nightly build -Z build-std=core --target x86_64-unknown-none.json --release"
