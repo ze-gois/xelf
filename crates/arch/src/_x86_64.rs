215664d915fcd246b8f5aa4443493df552f948d1
José Gois <ze.gois.00@gmail.com>
2025-05-28 18:41:55 -0300

    added pointer type n structure to arch

--- crates/arch/src/_x86_64.rs
+++ crates/arch/src/_x86_64.rs
@@ -1,4 +1,21 @@
 #![allow(unused)]
 mod callable;
+mod stacking;
 
 pub use callable::*;
+pub use stacking::*;
+
+pub type _Pointer = *mut u64;
+#[derive(Clone, Copy)]
+pub struct Pointer(pub _Pointer);
+
+pub type _Byte = *mut u8;
+pub struct Byte(pub _Byte);
+
+impl Pointer {
+    pub fn current() -> Self {
+        let _pointer: _Pointer;
+        unsafe { core::arch::asm!("mov {}, rsp", out(reg) _pointer) };
+        Pointer(_pointer)
+    }
+}
---
+++ crates/arch/src/_x86_64/stacking.rs
@@ -0,0 +1,4 @@
+impl crate::Stacking for crate::Arch {
+    type _Pointer = *mut u64;
+    // arch specific
+}
